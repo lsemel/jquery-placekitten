860851142286577f9e12720e6617900db4bc270e
Lee Semel <lee@semel.net>
2011-03-03 11:58:51 -0500

    Enable background for div, a

--- js/jquery.placekitten.js
+++ js/jquery.placekitten.js
@@ -1,26 +1,26 @@
 (function($) {
     $.fn.placekitten = function() {
-		    this.find('img,a,div').each( function(i,element) {
-    		    switch(element.nodeName) {
-        		    case 'IMG':
-            		    var w=element.width;
-                    var h=element.height;
-                        
+        this.find('img,a,div').each( function(i,element) {
+            var w, h;
+            switch(element.nodeName) {
+                case 'IMG':
+                    w = element.width;
+                    h = element.height;
                     if(w&&h) {
-                	    var src='http://placekitten.com/'+w+'/'+h;
-                      element.src=src;
-                  	}
+                        var src='http://placekitten.com/'+w+'/'+h;
+                        element.src=src;
+                    }
                     break;
-								case 'A':
+                case 'A':
                 case 'DIV':
-                		var background = $(element).css("background-image");
+                    var background = $(element).css("background-image");
                     if(background.indexOf('url(') > -1) {
-                    		var w = $(element).width();
-                        var h = $(element).height();
+                        w = $(element).width();
+                        h = $(element).height();
                         $(element).css("background-image", "url(http://placekitten.com/"+w+"/"+h+")");
                     }
                     break;
-                }
-            });
-		};
+            }
+        });
+    };
 })(jQuery);
