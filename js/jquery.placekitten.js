c03100e000b631f37955677875580de33f796591
Max Stockner <mail@bluemaex.de>
2011-03-03 11:01:44 +0100

    Added replacement of css background images on anchors and divs

--- js/jquery.placekitten.js
+++ js/jquery.placekitten.js
@@ -1,12 +1,26 @@
 (function($) {
     $.fn.placekitten = function() {
-       this.find('img').each(function(i,img) {
-           var w = img.width;
-           var h = img.height;
-           if (w && h) {
-               var src =  'http://placekitten.com/'+w+'/'+h;
-               img.src = src;
-           }
-       });
-    };
+		    this.find('img,a,div').each( function(i,element) {
+    		    switch(element.nodeName) {
+        		    case 'IMG':
+            		    var w=element.width;
+                    var h=element.height;
+                        
+                    if(w&&h) {
+                	    var src='http://placekitten.com/'+w+'/'+h;
+                      element.src=src;
+                  	}
+                    break;
+								case 'A':
+                case 'DIV':
+                		var background = $(element).css("background-image");
+                    if(background.indexOf('url(') > -1) {
+                    		var w = $(element).width();
+                        var h = $(element).height();
+                        $(element).css("background-image", "url(http://placekitten.com/"+w+"/"+h+")");
+                    }
+                    break;
+                }
+            });
+		};
 })(jQuery);
